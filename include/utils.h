464f2cf858c2bd6749a35ce45383e564ee77445c
ViniciusMiranda <viniciusmedeiros10.05@gmail.com>
2021-04-10 18:59:46 -0300

    feat(utils): adding more utility functions

--- include/utils.h
+++ include/utils.h
@@ -5,9 +5,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+
+#include <sys/stat.h>
+
 #include <ncurses.h>
 
-#include <string.h>
 
 // macros
 #define getName(var, str) sprintf(str, "%s", #var) 
@@ -15,5 +22,15 @@
 bool equals(const char* str1, const char *str2);
 
 bool str_array_contains(const char* str, char* str_array[], int array_size);
+
+bool file_exists(char *filename);
+
+bool dir_exists(char *dir_name);
+
+int set_last_char(char* str, char c, int size);
+
+int substr_index(char* str, char* sub_str);
+
+int replace_str(char* str, char* sub_str, char* rp_str);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -15,3 +15,43 @@ bool str_array_contains(const char* str,char* str_array[], int array_size) {
     return false;    
 }
 
+bool file_exists(char *filename) {
+  struct stat  buffer;   
+  return (stat (filename, &buffer) == 0);
+}
+
+int set_last_char(char* str, char c, int size) {
+    if(!str || !c) return ERR;
+    if(strlen(str) >= size) return ERR;
+
+    str[strlen(str)] = c;
+}
+
+bool dir_exists(char *dir_name);
+
+int substr_index(char* str, char* sub_str) {
+    int str_len = strlen(str), sub_str_len = strlen(sub_str);
+    if(str_len < sub_str_len) return ERR;
+
+    int begining = -1;
+    for(int i = 0; i < str_len && str_len + i <= sub_str_len; i++) {
+        if(equals(&str[i], sub_str)) begining = i; 
+    }
+
+    return begining;
+}
+
+int replace_str(char* str, char* sub_str, char* rp_str) {
+    if(!str || !sub_str || !rp_str) return ERR;
+
+    int str_len = strlen(str), sub_str_len = strlen(sub_str);
+    if(str_len < sub_str_len) return ERR;
+
+    int begining = substr_index(str, sub_str);
+    if(begining == ERR) return ERR;
+    if(sub_str_len > strlen(&str[begining])) return ERR;
+
+    for(int i = 0; i < sub_str_len + 1; i++) {
+        str[begining + i] = sub_str[i];
+    }
+}
