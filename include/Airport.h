07c7feecfaa3a85fea58d2f8e647cef9f4b85cb0
ViniciusMiranda <viniciusmedeiros10.05@gmail.com>
2021-05-02 17:37:06 -0300

    feat(airport): working on implementation of airport

--- include/Airport.h
+++ include/Airport.h
@@ -6,20 +6,48 @@
 
 #include "linked_list.h"
 
+#include "Database.h"
+#include "log.h"
+
 #include "Location.h"
 #include "WeatherCondition.h"
+#include "Plane.h"
 
-typedef linked_list_t* Planes_t;
+#define TABLE_NAME_AP "airport"
+
 typedef linked_list_t* Connections_t;
+
 
 typedef struct Airport {
     long int id;
-    char* name[AIRPORT_NAME_MAX];
-    char* code[AIRPORT_CODE_MAX];
+    char name[AIRPORT_NAME_MAX];
+    char code[AIRPORT_CODE_MAX];
     Location_t* location;
-    Planes_t planes; // planes are not implemented yet
-    Connections_t connections; // stores type Airport
+    Planes_t planes; 
+
+    /* linked list of airports */
+    Connections_t connections;
     WeatherCondition_t* WeatherCondition;
 } Airport_t;
+
+typedef linked_list_t* Airports_t;
+
+Airport_t* create_ap(char* name, char* code, Location_t* loc);
+
+int destroy_ap(Airport_t* ap);
+
+/* search functions */
+bool find_airport_by_name(void* d, void* cmp);
+
+bool find_airport_by_code(void* d, void* cmp);
+
+/* data persitance functions */
+int write_ap(Airport_t* ap);
+
+Airports_t read_ap();
+
+int init_ap_table();
+
+int getTablePath_ap();
 
 #endif
--- src/Airport.c
+++ src/Airport.c
@@ -1,1 +1,16 @@
 #include "Airport.h"
+
+bool find_airport_by_name(void* d, void* cmp) {
+    Airport_t* ap = (Airport_t*) d, *cmp_ap = (Airport_t*) cmp;
+    return equals(ap->name, cmp_ap->name); 
+}
+
+bool find_airport_by_code(void* d, void* cmp) {
+    Airport_t* ap = (Airport_t*) d, *cmp_ap = (Airport_t*) cmp;
+    return equals(ap->code, cmp_ap->code); 
+}
+
+int getTablePath_ap(char* dest) {
+    getTablePath_db(dest, TABLE_NAME_AP);
+}
+
